18732a53b60f684c8034a84637a2a1a902116b4d
Windfarer <windfarer@gmail.com>
2017-06-23 10:52:26 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # redis-token-bucket
+
+## Usage:
+```
+from redis_token_bucket import TokenBucketManager
+
+tbm = TokenBucketManager(redis_url="redis://path-to-your-redis", default_rate=10)
+# default_rate means how many tokens will be generated per second. 
+
+ok = tbm.get_token("my_key")
+# if token avaliable, return True, else return False
+
+```
+
+## TODO
+* burst rate
---
+++ redis_token_bucket.py
@@ -0,0 +1,54 @@
+import redis
+import logging
+import time
+
+
+class TokenBucketManager(object):
+    def __init__(self, redis_url="redis://localhost", default_rate=5):
+        self._redis_conn = redis.StrictRedis.from_url(url=redis_url)
+        ok = True
+        try:
+            ok = self._redis_conn.ping()
+        except redis.ConnectionError:
+            logging.warning("Redis is not available, TokenBucket will not work")
+            self._redis_conn = None
+        if not ok:
+            self._redis_conn = None
+        self._default_rate = default_rate
+        self._default_init_rate = default_rate - 1  # use one token after init
+
+    def get_token(self, key):
+        if not self._redis_conn:
+            logging.warning("Redis is not available, return True")
+            return True
+        tk = self._redis_conn.hget(key, "tk")
+        if tk is None:
+            tk = self._create_bucket(key)
+        else:
+            tk = int(tk)
+        if tk < 1:
+            tk = self._check_and_refill(key)
+            if tk < 0:
+                return False
+        else:
+            self._redis_conn.hset(key, "tk", tk - 1)
+        logging.debug(self._redis_conn.hget(key, "tk"))
+        return True
+
+    def _create_bucket(self, key):
+        logging.debug("create bucket")
+        tk = self._default_init_rate
+        ts = time.time()
+        self._redis_conn.pipeline().hset(key, "tk", tk).hset(key, "ts", ts).execute()
+        return tk
+
+
+    def _check_and_refill(self, key):
+        logging.debug("check and refill")
+        last_refill = float(self._redis_conn.hget(key, "ts"))
+        if last_refill + 1 < time.time():
+            tk = self._default_init_rate
+            self._redis_conn.pipeline().hset(key, "tk", tk).hset(key, "ts", time.time()).execute()
+            return tk
+        else:
+            return -1
